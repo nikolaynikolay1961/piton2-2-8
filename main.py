583da498bc7233f82c182590223139b11eaae84c
nikolay1961-22-10 <scherbakovnikolay61@gmail.com>
2022-11-24 20:08:55 +0700

    com main

--- main.py
+++ main.py
@@ -1007,7 +1007,6 @@
 # print('*'.join(st.split()))
 import re
 
-
 # s = 'Я ищу совпадения в 2023 году. И я их найду в 2 счёта'
 # reg = 'я'
 # reg1 = 'совпадения'
@@ -1256,6 +1255,66 @@ import re
 #
 # d_m_year = input('Введите дату, месяц, год в формате dd-mm-yyyy: ')
 # data(d_m_year)
+
+#
+# text = """
+# Самара
+# Москва
+# Тверь
+# Уфа
+# Казань
+# """
+# count = 0
+#
+#
+# def replace_find(m):
+#     global count
+#     count += 1
+#     return f"<option value='{count}'>{m.group(1)}</option>\n"
+#
+#
+# print(re.sub(r"\s*(\w+)\s*", replace_find, text))
+
+
+# s = "<p>Изображение <ing scr='bg.jpg'> - фон страницы</p>"
+# # reg = r'<ing\s+[^>]*scr=([\'"])(.+)\1>'
+# reg = r'<ing\s+[^>]*scr=(?P<q>[\'"])(.+)(?P=q)>'
+# print(re.findall(reg, s))
+
+# s = "Самолёт прилетает 10/23/2023. Будем рады вас видеть 10/24/2023"
+# reg = r'(\d{2})/(\d{2})/(\d{4})'
+# print(re.findall(reg, s))
+# print(re.sub(reg, r'\2.\1.\3', s))
+
+# s = "google.com and google.ru and yandex.ru"
+# reg = r'(([a-z0-9-]{2,}\.)+[a-z]{2,4})'
+# print(re.sub(reg, r"http://\1", s))
+
+test = "1X, Text, ACC 123 A1B2C3"
+reg = r"(?:\d)*(?:^\W)*(?:\d)+"
+reg1 = r'(?:\A\d)'
+reg2 = r"\d{1}[^\d{1,3}^\w]"
+
+print(re.findall(reg,  test))
+print(re.findall(reg1,  test))
+print(re.findall(reg2,  test))
+
+
+# txt1 = "#START# tile #END#"
+# reg = r'[a-z]{4}'
+# print(re.findall(reg, txt1))
+#
+# reg2 = r'#[A-Z]+#'
+# print(re.sub(reg2, r'', txt1))
+
+# dig = "12_34__56"
+# reg = r'\A\d{2}'
+#
+# print(re.findall(reg, dig))
+
+
+
+
 
 
 
